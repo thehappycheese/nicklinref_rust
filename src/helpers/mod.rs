916b5fa8d4949d05cebc89169d526b6b35aa188b
thehappycheese <the.nicholas.archer@gmail.com>
2023-06-08 01:09:36 +0800

    remove unused ErrorWithDynamicMessage

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -7,8 +7,5 @@ pub use with_shared_data::with_shared_data;
 mod error_with_static_message;
 pub use error_with_static_message::ErrorWithStaticMessage;
 
-mod error_with_dynamic_message;
-pub use error_with_dynamic_message::ErrorWithDynamicMessage;
-
 mod unit_conversion;
 pub use unit_conversion::convert_metres_to_degrees;
