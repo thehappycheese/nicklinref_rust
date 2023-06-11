c725b41c94a88afd2f7aed63c74ccbff214fbbbd
thehappycheese <the.nicholas.archer@gmail.com>
2023-06-11 17:03:22 +0800

    improve startup terminal output

--- src/data/indexed_data.rs
+++ src/data/indexed_data.rs
@@ -25,7 +25,7 @@ impl IndexedData {
     ) -> Result<Self, Box<dyn Error>>{
 
         let data = Self::read_or_update_cache_data(path_to_data_cache_file,url_to_download_new_data,force_update).await?;
-        println!("Indexing data");
+        println!("INFO: Indexing data");
         let index = index_data(&data)?;
         return Ok(Self{
             data,
@@ -75,46 +75,74 @@ impl IndexedData {
         let path_to_data_cache_file = Path::new(path_to_data_cache_file);
     
         if !force_update {
+            println!("INFO: Loading data from '{}'.", path_to_data_cache_file.display());
             match Self::load_data_from_file(path_to_data_cache_file) {
-                Ok(response) => return Ok(response),
-                Err(error_message) => println!("WARNING:Error while loading data: {}", error_message),
+                Ok(loaded_data) => return Ok(loaded_data),
+                Err(error_message) => println!("WARNING: Could not open the specified data file because '{}'. Will try to download fresh data.", error_message),
             }
         }
     
         if let Some(parent) = path_to_data_cache_file.parent() {
             if !parent.is_dir() {
-                std::fs::create_dir_all(parent)?;
+                println!("WARNING: The directory '{}' does not exist. Creating it now", parent.display());
+                match std::fs::create_dir_all(parent) {
+                    Ok(_) => (),
+                    Err(error_message) => {
+                        println!("WARNING: Could not create the directory '{}' because '{}'", parent.display(), error_message);
+                    }
+                }
             }
         }
     
         if path_to_data_cache_file.is_file() {
-            std::fs::remove_file(path_to_data_cache_file)?;
+            println!("WARNING: The file '{}' already exists. Attempting to delete it", path_to_data_cache_file.display());
+            match std::fs::remove_file(path_to_data_cache_file) {
+                Ok(_) => (),
+                Err(error_message) => {
+                    println!("WARNING: Could not delete the file '{}' because '{}'", path_to_data_cache_file.display(), error_message);
+                }
+            };
         }
         
-        println!("Downloading fresh data........");
+        
+        println!("INFO: Downloading fresh data");
+        println!("INFO: Using '{}'", url_to_download_new_data);
+        println!("INFO: Please wait, this can take some time...");
         let new_data = Self::download_data(url_to_download_new_data).await?;
         
-        println!("Saving data");
-        let file_out = File::create(path_to_data_cache_file)?;
-        let res = serde_json::to_vec(&new_data)?;
-        let compressor = lz_fear::framed::CompressionSettings::default();
-        compressor.compress(&res[..], &file_out)?;
+        println!("INFO: Saving data to file.");
+        match Self::save_data_to_file(path_to_data_cache_file, &new_data){
+            Ok(_) => (),
+            Err(error_message) => {
+                println!("WARNING: Could not save the specified data file '{}' because '{}'", path_to_data_cache_file.display(), error_message);
+                println!("WARNING: Data will be downloaded again next time the program is run");
+            }
+        }
+        
     
         Ok(new_data)
     }
 
     async fn download_data(
         url:&String,
-    ) -> Result<LayerSaved, Box<dyn std::error::Error>> {
+    ) -> Result<LayerSaved, Box<dyn Error>> {
         
         let mut document_to_save = LayerSaved {
             features: Vec::with_capacity(183_000),
         };
 
         let mut offset: usize = 0;
-        
+        const MAX_LOOPS: usize = 500;
+        let mut loop_safety_limit = MAX_LOOPS;
         loop {
-            print!(".");
+            loop_safety_limit -= 1;
+            if loop_safety_limit <= 0 {
+                return Err(
+                    //Box::new(ErrorWithStaticMessage::new(format!("Download failed. Loop safety limit of '{}' exceeded.", MAX_LOOPS).as_str()))
+                    Box::new(ErrorWithStaticMessage::new("Download failed. Loop safety limit exceeded."))
+                );
+            }
+
             let url = format!("{}&resultOffset={}", url, offset);
             let json: LayerDownloadChunk = reqwest::get(url).await?.json().await?;
             offset += json.features.len();
@@ -128,20 +156,28 @@ impl IndexedData {
                 break;
             }
         }
-
-        println!("Download completed. Sorting data.");
+        
+        println!("INFO: Download completed. Sorting data.");
         document_to_save
             .features
             .sort_by(|a, b| a.attributes.cmp(&b.attributes));
         Ok(document_to_save)
     }
 
-    fn load_data_from_file(s:&Path) -> Result<LayerSaved, Box<dyn std::error::Error>> {
-        println!("Loading data from file.");
-        let file = File::open(s)?;
+    fn load_data_from_file(file_path:&Path) -> Result<LayerSaved, Box<dyn Error>> {
+        let file = File::open(file_path)?;
         let lz_frame_reader = lz_fear::framed::LZ4FrameReader::new(file)?;
         let lz_frame_io_reader = lz_frame_reader.into_read();
-        let result: LayerSaved = serde_json::from_reader(lz_frame_io_reader)?;
+        let result = serde_json::from_reader(lz_frame_io_reader)?;
         Ok(result)
     }
+
+    fn save_data_to_file(file_path:&Path, data:&LayerSaved) -> Result<(), Box<dyn Error>> {
+        let file_out = File::create(file_path)?;
+        let res = serde_json::to_vec(&data)?;
+        let compressor = lz_fear::framed::CompressionSettings::default();
+        let result = compressor.compress(&res[..], &file_out)?;
+        Ok(result)
+    }
+
 }
