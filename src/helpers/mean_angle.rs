0446b48c11b02479ba9a247b4b44c54e42851990
thehappycheese <the.nicholas.archer@gmail.com>
2023-06-15 16:20:48 +0800

    test mean angle

--- src/helpers/mean_angle.rs
+++ src/helpers/mean_angle.rs
@@ -6,3 +6,35 @@ pub fn mean_angle(angles: Vec<f64>) -> f64 {
     let sin_mean: f64 = angles.iter().fold(0.0, |sum, i| sum + i.sin()) / length;
     (sin_mean).atan2(cos_mean)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::f64::consts::PI;
+
+    #[test]
+    fn test_mean_angle_zeros() {
+        let avg = mean_angle(vec![0.0, 0.0, 0.0]);
+        assert_eq!(avg, 0.0);
+    }
+
+    #[test]
+    fn test_mean_angle_180s() {
+        let avg = mean_angle(vec![
+            PI,
+            -PI,
+            PI,
+            PI
+        ]);
+        assert!(avg.abs()-PI < 0.0001);
+    }
+
+    #[test]
+    fn test_mean_angle_near_negative_180() {
+        let avg = mean_angle(vec![
+            f64::to_radians(-170.0),
+            f64::to_radians(160.0)
+        ]);
+        assert!(f64::to_degrees(avg) - 175.0 < 0.0001);
+    }
+}
